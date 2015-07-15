8d0309c2cb6e5109c3725ec503599b929bdba90b
Peti <antalpeti@users.noreply.github.com>
2015-07-15 07:24:05 +0200

    Add solution for arrayCount9 problem with test.

--- src/com/codingbat/warmup2/Warmup2.java
+++ src/com/codingbat/warmup2/Warmup2.java
@@ -137,4 +137,20 @@ public class Warmup2 {
     }
     return count;
   }
+
+  /**
+   * Given an array of ints, return true if one of the first 4 elements in the array is a 9. The
+   * array length may be less than 4.
+   *
+   * @param nums the input numbers
+   * @return true, if one of the first 4 elements in the array is a 9
+   */
+  public boolean arrayFront9(int[] nums) {
+    for (int i = 0; i < nums.length && i < 4; i++) {
+      if (nums[i] == 9) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
